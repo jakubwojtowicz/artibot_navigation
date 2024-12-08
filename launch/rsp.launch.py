ddbb1541228e1af5b17bd11b1ba25a7200d1ba03
jw185353 <jw185353@ROS.BASE.myguest.virtualbox.org>
2024-12-08 19:39:11 +0100

    update xacro

--- launch/rsp.launch.py
+++ launch/rsp.launch.py
@@ -5,7 +5,7 @@ from ament_index_python.packages import get_package_share_directory
 
 from launch import LaunchDescription
 from launch.substitutions import LaunchConfiguration
-from launch.actions import DeclareLaunchArgument
+from launch.actions import DeclareLaunchArgument, ExecuteProcess
 from launch_ros.actions import Node
 
 import xacro
@@ -18,11 +18,11 @@ def generate_launch_description():
 
     # Process the URDF file
     pkg_path = os.path.join(get_package_share_directory('artibot_navigation'))
-    xacro_file = os.path.join(pkg_path,'description','robot.urdf.xacro')
+    xacro_file = os.path.join(pkg_path, 'description', 'robot.urdf.xacro')
     robot_description_config = xacro.process_file(xacro_file)
     
     # Create a robot_state_publisher node
-    params = {'robot_description': robot_description_config.toxml(), 'use_sim_time': use_sim_time} 
+    params = {'robot_description': robot_description_config.toxml(), 'use_sim_time': use_sim_time}
     node_robot_state_publisher = Node(
         package='robot_state_publisher',
         executable='robot_state_publisher',
@@ -30,13 +30,36 @@ def generate_launch_description():
         parameters=[params]
     )
 
+    # Add rplidar_ros node
+    rplidar_node = ExecuteProcess(
+        cmd=[
+            'ros2', 'run', 'rplidar_ros', 'rplidar_composition',
+            '--ros-args',
+            '-p', 'serial_port:=/dev/serial/by-id/usb-Silicon_Labs_CP2102_USB_to_UART_Bridge_Controller_0001-if00-port0',
+            '-p', 'frame_id:=laser_joint',
+            '-p', 'angle_compensate:=true',
+            '-p', 'scan_mode:=Standard',
+            '-p', 'serial_baudrate:=115200'
+        ],
+        output='screen'
+    )
+
+    # Add rf2o_laser_odometry launch
+    rf2o_odometry_launch = ExecuteProcess(
+        cmd=[
+            'ros2', 'launch', 'rf2o_laser_odometry', 'rf2o_laser_odometry.launch.py'
+        ],
+        output='screen'
+    )
 
     # Launch!
     return LaunchDescription([
         DeclareLaunchArgument(
             'use_sim_time',
             default_value='false',
-            description='Use sim time if true'),
-
-        node_robot_state_publisher
+            description='Use sim time if true'
+        ),
+        node_robot_state_publisher,
+        rplidar_node,
+        rf2o_odometry_launch
     ])
